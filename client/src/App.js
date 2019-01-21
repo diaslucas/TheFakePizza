ca3581d089a7ac2a2f984a28258f171d7ca2f3cd
diaslucas <diaslucas>
2019-01-20 23:06:24 -0500

    Implementing Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import { Row, Navbar, NavItem } from 'react-materialize'
 import './App.css';
 import Home from './components/Home';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        pizzas {
+          flavor
+          price
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 class App extends Component {
   render() {
