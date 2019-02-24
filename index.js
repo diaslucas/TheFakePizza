74ba5ae9ed85845c8f39c406f6e3304ad3a115b7
diaslucas <diaslucas>
2019-02-24 12:39:26 -0500

    Changes to deploy

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ const server = new ApolloServer({
 
 server.applyMiddleware({app})
 
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+
 const port = process.env.PORT || PORT
 
 app.listen({ port }, () =>
